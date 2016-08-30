db85730f13cf316a99914590f5e60085cb151f07
Dr. Stefan Reisner <stefan.reisner@syngenio.de>
2016-08-30 12:03:38 +0200

    added

---
+++ com.herenow/src/main/java/com/herenow/MongoDBAuthenticationProvider.java
@@ -0,0 +1,42 @@
+package com.herenow;
+
+import org.jongo.MongoCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import ch.qos.logback.core.net.server.Client;
+
+@Service
+public class MongoDBAuthenticationProvider extends AbstractUserDetailsAuthenticationProvider {
+
+    @Autowired
+    private MongoCollection users;
+
+    @Override
+    protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+    }
+
+    @Override
+    protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+        UserDetails loadedUser;
+
+        try {
+            Client client = users.findOne("{#: #}", Client.USERNAME, username).as(Client.class);
+            loadedUser = new User(client.getUsername(), client.getPassword(), client.getRoles());
+        } catch (Exception repositoryProblem) {
+            throw new InternalAuthenticationServiceException(repositoryProblem.getMessage(), repositoryProblem);
+        }
+
+        if (loadedUser == null) {
+            throw new InternalAuthenticationServiceException(
+                    "UserDetailsService returned null, which is an interface contract violation");
+        }
+        return loadedUser;
+    }
+}
